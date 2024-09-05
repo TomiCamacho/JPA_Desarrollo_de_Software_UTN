99a8c0d6d403bb8ab8c4c07ee246d307f7e00a22
tomy <tomycamacho3004@gmail.com>
2024-09-04 22:25:11 -0300

    Relaciones entre clases terminadas.

--- src/main/java/main/PersistenceApp.java
+++ src/main/java/main/PersistenceApp.java
@@ -6,16 +6,30 @@ import javax.persistence.Persistence;
 
 import org.example.*;
 
+import java.util.List;
+
 public class PersistenceApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("example-unit");
         EntityManager em = emf.createEntityManager();
 
-        try{
+        try {
             em.getTransaction().begin();
 
-            Cliente cliente = new Cliente("Tomás","Camacho", 45719267);
+            Cliente cliente = new Cliente("Tomás", "Camacho", 45719267);
+            Domicilio domicilio = new Domicilio("Tucuman", 76);
+
+            cliente.setDomicilio(domicilio);
+            domicilio.setCliente(cliente);
+
             em.persist(cliente);
+
+            Domicilio dom = em.find(Domicilio.class, 1L);
+            Cliente cli = em.find(Cliente.class, 1L);
+
+            System.out.println("Cliente de domicilio: " + dom.getCliente().getDni());
+            System.out.println("Domicilio de cliente: " + cli.getDomicilio().getNombreCalle());
+
             em.flush();
             em.getTransaction().commit();
 
